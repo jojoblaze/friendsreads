5372a76a04e8012536e2cfb3e2a92e59dabb27cc
Orologio <matteo.delloioio@gmail.com>
2019-11-24 20:19:24 +0100

    add simplified models and retrieve book review rate

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ var router = express.Router();              // get an instance of the express Ro
 
 
 
+let goodReadsUser = { id: null, name: null, read_books: [], reading_books: [] }
 
 
 const myCredentials = {
@@ -84,10 +85,53 @@ router.get('/goodreads_callback', async function (req, res) {
 
     let userData = await gr.getCurrentUserInfo();
 
+    goodReadsUser.id = userData.user.id;
+    goodReadsUser.name = userData.user.name;
+
     console.log(userData.user.id);
     let readBooks = await gr.getBooksOnUserShelf(userData.user.id, 'read');
     let currentlyReadingBooks = await gr.getBooksOnUserShelf(userData.user.id, 'currently-reading');
-    res.json({ user: userData, readBooks: readBooks, currentlyReadingBooks: currentlyReadingBooks });
+
+
+    for(let i = 0; i < readBooks.books.book.length; i++) {
+        
+        let book = { id: null, isbn: null, isbn13: null, title: null, small_image_url: null, num_pages: null, authors: [], rates: null };
+
+        book.id = readBooks.books.book[i].id._;
+        book.isbn = readBooks.books.book[i].isbn;
+        book.isbn13 = readBooks.books.book[i].isbn13;
+        book.title = readBooks.books.book[i].title;
+        book.small_image_url = readBooks.books.book[i].small_image_url;
+        book.num_pages = readBooks.books.book[i].num_pages;
+
+        
+        // for(let a = 0; a < readBooks.books.book[i].authors.author.length; a++) {
+        //     let author = { id: null, name: null }
+        //     author.id = readBooks.books.book[i].authors.author[a].id;
+        //     author.name = readBooks.books.book[i].authors.author[a].name;
+        //     book.authors.push(author);
+        // }
+
+        // author mapping
+        let author = { id: null, name: null }
+        author.id = readBooks.books.book[i].authors.author.id;
+        author.name = readBooks.books.book[i].authors.author.name;
+        
+        // /////
+
+        // console.log(book);
+        let userReview = await gr.getUsersReviewForBook(goodReadsUser.id, book.id);
+
+        // console.log(userReview);
+        book.rates = userReview.review.rating;
+        book.authors.push(author);
+
+
+        goodReadsUser.read_books.push(book);
+    }
+
+
+    res.json({ user: goodReadsUser, readBooks: goodReadsUser.read_books, currentlyReadingBooks: currentlyReadingBooks });
 
 });
 
